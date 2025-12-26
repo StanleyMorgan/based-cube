eb49d1d1740f27ac940fd593ce3ae7a4861e9d6c
stmorgan.eth <127997825+StanleyMorgan@users.noreply.github.com>
2025-12-26 16:49:26 +0300

    Rename BackIcon to BottomIcon

--- components/icons/BottomIcon.tsx
+++ components/icons/BottomIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CandyCane } from 'lucide-react';
 
-export const BackIcon = () => (
+export const BottomIcon = () => (
     <CandyCane size={40} strokeWidth={0.5} className="opacity-30" />
 );
