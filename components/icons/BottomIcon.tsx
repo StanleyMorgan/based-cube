02b047c20085b4891510b4b414738492d4663182
stmorgan.eth <127997825+StanleyMorgan@users.noreply.github.com>
2026-01-21 19:06:14 +0300

    Replace CandyCane icon with Atom icon

--- components/icons/BottomIcon.tsx
+++ components/icons/BottomIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { CandyCane } from 'lucide-react';
+import { Atom } from 'lucide-react';
 
 export const BottomIcon = () => (
-    <CandyCane size={50} strokeWidth={1} className="opacity-30" />
+    <Atom size={50} strokeWidth={1} className="opacity-30" />
 );
