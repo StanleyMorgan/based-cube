80b5a86e21879c83f16b1d417dc884468ef926e7
stmorgan.eth <127997825+StanleyMorgan@users.noreply.github.com>
2026-01-21 19:05:44 +0300

    Replace TreePine icon with Rocket icon

--- components/icons/BackIcon.tsx
+++ components/icons/BackIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { TreePine } from 'lucide-react';
+import { Rocket } from 'lucide-react';
 
 export const BackIcon = () => (
-    <TreePine size={50} strokeWidth={1} className="opacity-30" />
+    <Rocket size={50} strokeWidth={1} className="opacity-30" />
 );
