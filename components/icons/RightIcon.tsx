8f5ad9d0be920ad5506340171d7c5fa8f0b7f536
stmorgan.eth <127997825+StanleyMorgan@users.noreply.github.com>
2026-01-21 19:12:36 +0300

    Replace Trophy icon with Gem icon in RightIcon

--- components/icons/RightIcon.tsx
+++ components/icons/RightIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Trophy } from 'lucide-react';
+import { Gem } from 'lucide-react';
 
 export const RightIcon = () => (
-    <Trophy size={50} strokeWidth={1} className="opacity-30" />
+    <Gem size={50} strokeWidth={1} className="opacity-30" />
 );
